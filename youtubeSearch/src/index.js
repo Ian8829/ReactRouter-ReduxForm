26b4b4ea298e4d6c412d2d6c6a7b7d2e98101c85
Ian8829 <ghjkghj@naver.com>
2017-06-07 11:51:34 +0900

    Exports Statements

--- youtubeSearch/src/index.js
+++ youtubeSearch/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyD5csg_18zq0N-PUuQk_YwrIhIzpzifNAk';
+
 // Create a new comp.
 const App = function () {
-    return <div>Hi!</div>
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
 };
 
 
