3244a1cd7ef6a9236e585c07f433b7c84054f81f
Ian8829 <ghjkghj@naver.com>
2017-06-07 13:19:16 +0900

    video list

--- youtubeSearch/src/index.js
+++ youtubeSearch/src/index.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
+import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyD5csg_18zq0N-PUuQk_YwrIhIzpzifNAk';
 
 class App extends Component {
@@ -21,6 +20,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar/>
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
