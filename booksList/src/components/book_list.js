2d4d8e8c18447b848c8f9960509131a4f9a8eca8
Ian8829 <ghjkghj@naver.com>
2017-06-07 15:32:34 +0900

    building action creators

--- booksList/src/components/book_list.js
+++ booksList/src/components/book_list.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
     renderList() {
@@ -23,10 +25,14 @@ class BookList extends Component {
     }
 }
 
-function mapStateToProps(state) {
+function mapStateToProps() {
     return {
         books: state.books
     };
 }
 
-export default connect(mapStateToProps)(BookList);
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ selectBook }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
