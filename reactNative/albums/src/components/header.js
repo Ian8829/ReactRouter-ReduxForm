015a2f814b74855a39a5da2fd300f71d254368d0
taeshik Hwang <ian@taeshiks-MacBook-Pro.local>
2017-06-14 14:35:20 +0900

    Styling more

--- reactNative/albums/src/components/header.js
+++ reactNative/albums/src/components/header.js
@@ -1,18 +1,24 @@
 // Import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make a component
 const Header = () => {
-  const { textStyle } = styles;
+  const { textStyle, viewStyle } = styles;
 
-  return <Text style={textStyle}>Albums!</Text>;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
+  );
 };
 
 const styles = {
+  viewStyle: {
+    backgroundColor: '#eaf8de'
+  },
   textStyle: {
-    fontSize: 20,
-    marginTop: 20
+    fontSize: 20
   }
 };
 
